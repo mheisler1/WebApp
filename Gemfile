7732e8f50ddf392124f0d77924047abbd19be898
Mitchell and Cole <mvolk1@student.cvtc.edu>
2014-11-19 20:11:53 -0600

    Changes index.html to blank page and adds devise gem

--- Gemfile
+++ Gemfile
@@ -39,3 +39,6 @@ gem 'spring',        group: :development
 # gem 'debugger', group: [:development, :test]
 
 gem 'tzinfo-data'
+
+# devise gem for login authentication
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :job_postings
   get 'job_posting/index'
 
-    root :to => "job_postings#new"
+    root :to => "job_postings#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

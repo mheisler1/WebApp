436599858ced38493d5f79c8c0fd406cb93f5e86
Mitchell Volk <mvolk1@student.cvtc.edu>
2014-11-29 15:41:39 -0600

    Adds working unit tests that use the fixtures from job_postings.yml
    One test is added for each of the form fields to test that the data
    entered should be filled in.
    
    One test is added for the search bar functionality per each field in
    the database with the exception of,  job_type, job_type_id, time_
    created, time_updated, and approved. The job_type fields should
    be placed in a separate test file.

--- test/models/job_posting_test.rb
+++ test/models/job_posting_test.rb
@@ -1,73 +1,120 @@
 require 'test_helper'
 
 class JobPostingTest < ActiveSupport::TestCase
+    #
+    # Test cases for job_posting form fields
+    #
     test "creates a valid record" do
-        posting = JobPosting.new
-        posting.title = "Software Developer"
-        posting.company = "Menards"
-        posting.job_type_id = 1
-        posting.salary = "$45,000"
-        posting.duties = "Creates new websites on the fly"
-        posting.basic_qualifications = "Current student"
-        posting.education = "Associates in Computer Science or equivalent experience"
-        posting.skills = "Working in a team environment"
-        posting.how_to_apply = "Go to www.Menards.com"
-        
+        posting = job_postings(:one)
+        assert_instance_of(JobPosting, posting, "All fields need to be filled in")
         assert posting.save
     end
     
     test "should not save unless title is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:title].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if title is blank
+        assert posting.errors[posting.title]
     end
     
     test "should not save unless company is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:company].include?("can't be blank")
-    end
-    
-    test "should not save unless job type is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:job_type_id].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if company is blank
+        assert posting.errors[posting.company]
     end
     
     test "should not save unless salary is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:salary].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if salary is blank
+        assert posting.errors[posting.salary]
     end
     
     test "should not save unless duties are filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:duties].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if duties are blank
+        assert posting.errors[posting.duties]
     end
     
     test "should not save unless basic qualifications are filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:basic_qualifications].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if basic_qualifications are blank
+        assert posting.errors[posting.basic_qualifications]
     end
     
     test "should not save unless education is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:education].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if education is blank
+        assert posting.errors[posting.education]
     end
     
     test "should not save unless skills are filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:skills].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if skills are blank
+        assert posting.errors[posting.skills]
     end
 
     test "should not save unless how to apply is filled in" do
-        posting = JobPosting.new
-        assert !posting.save # save should fail because there are errors. 
-        assert posting.errors[:how_to_apply].include?("can't be blank")
+        posting = job_postings(:one)
+        assert(posting.save, "cant be blank") # save will error if how_to_apply is blank
+        assert posting.errors[posting.how_to_apply]
+    end
+
+    #
+    # Tests for each field JobPosting search method
+    #
+    test "search should return a single job_posting with posting.title params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("Software Engineer"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.company params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("Target"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.salary params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("$45,000"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.duties params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("Build Software"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.basic_qualifications params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("0-6"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.education params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("Associates"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.skills params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("Java"), "Values should be equal")
+    end
+
+    test "search should return a single job_posting with posting.how_to_apply params" do
+        posting = job_postings(:one)
+        assert_equal([posting], JobPosting.search("www.target.com"), "Values should be equal")
+    end
+
+    #
+    # Tests for amount of job_postings to return
+    #
+    test "search should not return a job_posting with Web Developer params" do
+        posting = job_postings(:one)
+        assert_not_equal([posting], JobPosting.search("Web Developer"), "Values should not be equal")
+    end
+
+    test "search should return two job_postings with XML params" do
+        assert(JobPosting.search("XML").count == 2, "Two values in fixtures to return")
+    end
+
+    test "search should return all job_postings with no params" do
+        assert(JobPosting.search("").count == 2, "Two values in fixtures to return")
     end
     
 end

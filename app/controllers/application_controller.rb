6be73e1f1692f7bdb9572b47e0034704c57f2df9
Cole Butterfield <cbutterfield7@student.cvtc.edu>
2014-12-02 10:07:07 -0600

    Adds Employer Login
    Ran generate devise user
    
    Added Approved column to users table
    
    Over writes the model to display message if the acount has not yet been approved
    Sets messages is i18n file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
     before_filter :authenticate_admin!
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :company
+      devise_parameter_sanitizer.for(:account_update) << :company
+    end
+
+
+
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.

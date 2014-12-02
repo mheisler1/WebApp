6c3ddb57bf6eca0db52efc8fed5903814e37bb26
Cole Butterfield <cbutterfield7@student.cvtc.edu>
2014-12-02 11:14:37 -0600

    Comments out sanitizer
    Not sure if needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
     before_filter :authenticate_admin!
-    before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :company
-      devise_parameter_sanitizer.for(:account_update) << :company
-    end
+#    before_filter :configure_permitted_parameters, if: :devise_controller?
+#
+#    protected
+#
+#    def configure_permitted_parameters
+#      devise_parameter_sanitizer.for(:sign_up) << :company
+#      devise_parameter_sanitizer.for(:account_update) << :company
+#    end
 
 
 

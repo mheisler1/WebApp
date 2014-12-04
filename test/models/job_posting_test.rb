03b5aa919443711658723ab1400b958141ac2394
Matt <mheisler1@student.cvtc.edu>
2014-12-03 19:39:02 -0600

    Refactored and added test for search feature

--- test/models/job_posting_test.rb
+++ test/models/job_posting_test.rb
@@ -62,59 +62,114 @@ class JobPostingTest < ActiveSupport::TestCase
     # Tests for each field JobPosting search method
     #
     test "search should return a single job_posting with posting.title params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("Software Engineer"), "Values should be equal")
+        params = {:search => "Software Engineer"}
+        result = JobPosting.search(params)
+        assert_equal(1, result.count)
+        assert_equal("Software Engineer", result.first.title)
     end
 
     test "search should return a single job_posting with posting.company params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("Target"), "Values should be equal")
+        params = {:search => "Target"}
+        result = JobPosting.search(params)
+        assert_equal(1, result.count)
+        assert_equal("Target", result.first.company)
     end
 
     test "search should return a single job_posting with posting.salary params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("$45,000"), "Values should be equal")
+        params = {:search => "45,000"}
+        result = JobPosting.search(params)
+        assert_equal("$45,000", result.first.salary)
+        assert_equal(1, result.count)
     end
 
     test "search should return a single job_posting with posting.duties params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("Build Software"), "Values should be equal")
+        params = {:search => "Build Software"}
+        result = JobPosting.search(params)
+        assert_equal("Build software", result.first.duties)
+        assert_equal(1, result.count)
     end
 
     test "search should return a single job_posting with posting.basic_qualifications params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("0-6"), "Values should be equal")
+        params = {:search => "0-6"}
+        result = JobPosting.search(params)
+        assert_equal("0-6 months OOP experience", result.first.basic_qualifications)
+        assert_equal(1, result.count)
     end
 
     test "search should return a single job_posting with posting.education params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("Associates"), "Values should be equal")
+        params = {:search => "Associates"}
+        result = JobPosting.search(params)
+        assert_equal("Associates Degree", result.first.education)
+        assert_equal(1, result.count)
     end
 
     test "search should return a single job_posting with posting.skills params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("Java"), "Values should be equal")
+        params = {:search => "Java"}
+        result = JobPosting.search(params)
+        assert_equal("Java, XML, Eclipse IDE", result.first.skills)
+        assert_equal(1, result.count)
     end
 
     test "search should return a single job_posting with posting.how_to_apply params" do
-        posting = job_postings(:one)
-        assert_equal([posting], JobPosting.search("www.target.com"), "Values should be equal")
+        params = {:search => "www.target.com"}
+        result = JobPosting.search(params)
+        assert_equal("www.target.com", result.first.how_to_apply)
+        assert_equal(1, result.count)
     end
 
     #
     # Tests for amount of job_postings to return
     #
-    test "search should not return a job_posting with Web Developer params" do
-        posting = job_postings(:one)
-        assert_not_equal([posting], JobPosting.search("Web Developer"), "Values should not be equal")
+    test "search should not return a job_posting with abc params" do
+        params = {:search => "abc"}
+        result = JobPosting.search(params)
+        assert_equal(0, result.count)
     end
 
     test "search should return two job_postings with XML params" do
-        assert(JobPosting.search("XML").count == 2, "Two values in fixtures to return")
+        params = {:search => "XML"}
+        result = JobPosting.search(params)
+        assert_equal(2, result.count)
     end
 
     test "search should return all job_postings with no params" do
-        assert(JobPosting.search("").count == 2, "Two values in fixtures to return")
+        params = {:search => ""}
+        result = JobPosting.search(params)
+        assert_equal(2, result.count)
+    end
+    
+    test "search should return one job_posting with job_type of 2 in search params" do
+        params = {:job_type => 2}
+        result = JobPosting.search(params)
+        assert_equal(1, result.count)
+    end
+    
+    test "search should return two job_postings with pay_type of 1 in search params" do
+        params = {:pay_type => 1}
+        result = JobPosting.search(params)
+        assert_equal(2, result.count)
+    end
+    
+    test "search should return one job_postings with location_type of 2 in search params" do
+        params = {:location_type => 2}
+        result = JobPosting.search(params)
+        assert_equal(2, result.count)
+    end
+    
+    test "search should return one job_posting with pay_type of 1 and keyword of JB Systems" do
+        params = {:search => "JB Systems",
+                  :pay_type => 1}
+        result = JobPosting.search(params)
+        assert_equal(1, result.count)
+        assert_equal("JB Systems", result.first.company)
+    end
+
+    test "search should return one job_posting with location_type of 2 and job_type of 1" do
+        params = {:location_type => 2,
+                  :job_type => 1}
+        result = JobPosting.search(params)
+        assert_equal(1, result.count)
+        assert_equal("Web Developer", result.first.title)
     end
     
 end
